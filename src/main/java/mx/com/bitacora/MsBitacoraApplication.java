d5e8f1774cf22c748c9f8f8ca7761923770e85a4
Samuel Flores <samfg988@gmail.com>
2024-06-25 20:32:51 -0600

    feat: First commit

---
+++ src/main/java/mx/com/bitacora/MsBitacoraApplication.java
@@ -0,0 +1,13 @@
+package mx.com.bitacora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBitacoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBitacoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/bitacora/MsBitacoraApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.bitacora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBitacoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
